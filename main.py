d06d9e710292222213c47d96ba141f4d5b072c53
= <dhruvaakkineni79@gmail.com>
2023-03-31 19:28:06 -0500

    I remembered this time

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -9,3 +9,14 @@ def login():
 @app.route("/")
 def index():
     return render_template('index.html')
+
+@app.route("/calculator", methods=["GET", "POST"])
+def calc():
+    x = request.form.get('x')
+    y = request.form.get('y')
+    ans = 0
+    if x is not None and y is not None:
+        ans = int(x) + int(y)
+
+
+    return render_template("calc.html", ans=ans)
